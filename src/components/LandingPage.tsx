97d0423d87edb4b84067df8aa58769b2b4dbe2f2
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2025-10-24 15:53:02 -1000

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -27,11 +27,10 @@ export function LandingPage({ onStartQuiz, onLogin }: LandingPageProps) {
         </motion.div>
 
         <div className="space-y-4">
-          <div className="flex grid-cols-2 grid-rows-2 gap-4 text-5xl md:text-5xl font-bold leading-tight">
-            <h1> 
-              <span className="grid-col-1 items-center flex"> You've heard the buzz about </span>
-              <span><TypewriterText words={["Web3", "Crypto", "Blockchain", "Bitcoin"]} className="grid-col-2 justify-left flex"/></span>
-              <break></break>
+          <div className="flex items-center text-5xl md:text-5xl font-bold leading-tight">
+            <h1 className="grow-2"> You've heard the buzz about </span>
+              <span><TypewriterText words={["Web3", "Crypto", "Blockchain", "Bitcoin"]} className="grow-1"/></span>
+              <br></br>
               <span clasName="grid-row-2 flex justify-center">— now see what it really means for you. </span>
             </h1>
           </div>
