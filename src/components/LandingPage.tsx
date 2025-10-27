46c6f4ab9a613d412a4e34846f86f41d5991a405
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2025-10-26 21:42:53 -1000

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -28,7 +28,7 @@ export function LandingPage({ onStartQuiz, onLogin }: LandingPageProps) {
 
         <div className="space-y-4">
           <div className="text-gray-900">
-            You've heard the buzz about <TypewriterText words={["Web3", "Crypto", "Blockchain", "Bitcoin"]} class="bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent">
+            You've heard the buzz about <TypewriterText words={["Web3", "Crypto", "Blockchain", "Bitcoin"]} ">
             <br></br>
             — now see what it really means for you
           </div>
